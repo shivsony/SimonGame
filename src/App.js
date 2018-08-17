713bfef7c0c524b52bb5008d4a75522d4d7c47f8
Shivendra Verma <Shivendrasony@gmail.com>
2018-08-17 18:16:07 +0530

    basic structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
 import Header from './components/Header/Header';
+import Circle from './components/Circle/Circle';
+import img from './back.png';
 import './App.css';
-
+const style = {
+    backgraoundImage: "url("+ {img} +")"
+};
 class App extends Component {
   render() {
+      
     return (
       <div className="App">
         <Header/>
+        <div style={style} className="wrapper">
+            <Circle>
+              <Circle/>
+            </Circle>
+        </div>
       </div>
     );
   }
---
+++ src/components/Circle/Circle.js
@@ -0,0 +1,22 @@
+import React from 'react';
+const style = {
+    backgroundColor: "antiquewhite",
+    width: "35%",
+    margin: "0 auto",
+    borderRadius: "23rem",
+    border: "20px solid #363636",
+    height: "450px"
+}
+
+class Circle extends React.Component {
+    render() {
+        return(
+            <div style={style}>
+                hello
+            
+            </div>
+        )
+    };
+}
+
+export default Circle
